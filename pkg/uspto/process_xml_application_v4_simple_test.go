e0cf2a85bb17714ea5ada6a7fc778f1d87c96509
cconzen <104637624+cconzen@users.noreply.github.com>
2023-03-24 23:25:43 +0100

    test for another doc

--- pkg/uspto/process_xml_application_v4_simple_test.go
+++ pkg/uspto/process_xml_application_v4_simple_test.go
@@ -610,4 +610,186 @@ func TestApplicationProcessXMLSimpleVersion461A1(t *testing.T) {
 
 }
 
+func TestApplicationProcessXMLSimpleVersion462A1(t *testing.T) {
+	ass := assert.New(t)
+	data, err := ioutil.ReadFile("./test-data/application/US20230080311A1-20230316.XML")
+	ass.NoError(err)
+	patDoc, err := ProcessXMLSimple(data)
+	ass.NoError(err)
+
+	ass.Equal("20230080311", patDoc.ID)
+	ass.Equal("US20230080311A1-20230316.XML", patDoc.File)
+	ass.Equal("EN", patDoc.Lang)
+	ass.Equal(Country("US"), patDoc.Country)
+	ass.Equal("20230080311", patDoc.DocNumber)
+	ass.Equal("A1", patDoc.Kind)
+	ass.False(patDoc.DatePubl.IsZero())
+	ass.Equal("20230316", patDoc.DatePubl.Format(layoutDatePubl))
+	ass.Equal("PRODUCTION", patDoc.Status)
+	ass.Equal("v4.6 2022-02-17", patDoc.DtdVersion)
+
+	// title
+	ass.NotEmpty(patDoc.Title)
+	ass.Equal("Method of improving methyltransferase activity", patDoc.Title[0].Text)
+	ass.Equal("en", patDoc.Title[0].Language)
+
+	// abstract
+	ass.NotEmpty(patDoc.Abstract)
+	ass.Equal(273, len(patDoc.Abstract[0].Text))
+	ass.Equal("en", patDoc.Abstract[0].Language)
+
+	// claims
+	ass.NotEmpty(patDoc.Claims)
+	ass.Equal(7712, len(patDoc.Claims[0].Text))
+	ass.Equal("en", patDoc.Claims[0].Language)
+	ass.Equal("claims", patDoc.Claims[0].Id)
+
+	// description
+	ass.NotEmpty(patDoc.Description)
+	ass.Equal(107827, len(patDoc.Description[0].Text))
+	ass.Equal("en", patDoc.Description[0].Language)
+
+	ass.Empty(patDoc.Citations)
+
+	// inventors
+	ass.NotEmpty(patDoc.Inventors[0])
+	ass.Equal(Country("DK"), patDoc.Inventors[0].Country)
+	ass.Equal("Kongens Lyngby", patDoc.Inventors[0].City)
+	ass.Empty(patDoc.Inventors[0].Street)
+	ass.Equal("Luo, Hao", patDoc.Inventors[0].Name)
+	ass.Equal("Hao", patDoc.Inventors[0].FirstName)
+	ass.Equal("Luo", patDoc.Inventors[0].LastName)
+	ass.Equal("", patDoc.Inventors[0].State)
+
+	ass.NotEmpty(patDoc.Inventors[1])
+	ass.Equal(Country("DK"), patDoc.Inventors[1].Country)
+	ass.Equal("Kongens Lyngby", patDoc.Inventors[1].City)
+	ass.Empty(patDoc.Inventors[1].Street)
+	ass.Equal("Hansen, Anne Sofie Laerke", patDoc.Inventors[1].Name)
+	ass.Equal("Anne Sofie Laerke", patDoc.Inventors[1].FirstName)
+	ass.Equal("Hansen", patDoc.Inventors[1].LastName)
+	ass.Equal("", patDoc.Inventors[1].State)
+
+	// Classifications
+	ass.Equal(14, len(patDoc.Classifications))
+
+	for i := 0; i <= 13; i++ {
+		ass.Equal("C", patDoc.Classifications[i].Section)
+		ass.Equal("12", patDoc.Classifications[i].Class)
+		ass.Equal("20230316", patDoc.Classifications[i].ActionDate)
+		ass.Equal("B", patDoc.Classifications[i].OriginalOrReclassified)
+		ass.Equal("H", patDoc.Classifications[i].Source)
+		ass.Equal("US", patDoc.Classifications[i].GeneratingOffice)
+	}
+	for i := 0; i <= 3; i++ {
+		ass.Equal(ClassificationSystem("IPC"), patDoc.Classifications[i].System)
+		ass.Equal(i+1, patDoc.Classifications[i].Sequence)
+		ass.Equal("A", patDoc.Classifications[i].ClassificationLevel)
+	}
+	for i := 4; i <= 13; i++ {
+		ass.Equal(ClassificationSystem("CPC"), patDoc.Classifications[i].System)
+		ass.Equal(i-3, patDoc.Classifications[i].Sequence)
+		ass.Equal("", patDoc.Classifications[i].ClassificationLevel)
+	}
+	// IPC
+
+	ass.Equal("N", patDoc.Classifications[0].SubClass)
+	ass.Equal("9", patDoc.Classifications[0].MainGroup)
+	ass.Equal("10", patDoc.Classifications[0].SubGroup)
+	ass.Equal("20060101", patDoc.Classifications[0].Version)
+	ass.Equal("F", patDoc.Classifications[0].FirstLater)
+	ass.Equal("I", patDoc.Classifications[0].ClassificationValue)
+
+	ass.Equal("N", patDoc.Classifications[1].SubClass)
+	ass.Equal("1", patDoc.Classifications[1].MainGroup)
+	ass.Equal("36", patDoc.Classifications[1].SubGroup)
+	ass.Equal("20060101", patDoc.Classifications[1].Version)
+	ass.Equal("L", patDoc.Classifications[1].FirstLater)
+	ass.Equal("I", patDoc.Classifications[1].ClassificationValue)
+
+	ass.Equal("N", patDoc.Classifications[2].SubClass)
+	ass.Equal("9", patDoc.Classifications[2].MainGroup)
+	ass.Equal("88", patDoc.Classifications[2].SubGroup)
+	ass.Equal("20060101", patDoc.Classifications[2].Version)
+	ass.Equal("L", patDoc.Classifications[2].FirstLater)
+	ass.Equal("I", patDoc.Classifications[2].ClassificationValue)
+
+	ass.Equal("N", patDoc.Classifications[3].SubClass)
+	ass.Equal("15", patDoc.Classifications[3].MainGroup)
+	ass.Equal("10", patDoc.Classifications[3].SubGroup)
+	ass.Equal("20060101", patDoc.Classifications[3].Version)
+	ass.Equal("L", patDoc.Classifications[3].FirstLater)
+	ass.Equal("I", patDoc.Classifications[3].ClassificationValue)
+	//CPC
+	ass.Equal("N", patDoc.Classifications[4].SubClass)
+	ass.Equal("9", patDoc.Classifications[4].MainGroup)
+	ass.Equal("1007", patDoc.Classifications[4].SubGroup)
+	ass.Equal("20130101", patDoc.Classifications[4].Version)
+	ass.Equal("F", patDoc.Classifications[4].FirstLater)
+	ass.Equal("I", patDoc.Classifications[4].ClassificationValue)
+
+	ass.Equal("Y", patDoc.Classifications[5].SubClass)
+	ass.Equal("201", patDoc.Classifications[5].MainGroup)
+	ass.Equal("01004", patDoc.Classifications[5].SubGroup)
+	ass.Equal("20130101", patDoc.Classifications[5].Version)
+	ass.Equal("L", patDoc.Classifications[5].FirstLater)
+	ass.Equal("I", patDoc.Classifications[5].ClassificationValue)
+
+	ass.Equal("N", patDoc.Classifications[6].SubClass)
+	ass.Equal("1", patDoc.Classifications[6].MainGroup)
+	ass.Equal("36", patDoc.Classifications[6].SubGroup)
+	ass.Equal("20130101", patDoc.Classifications[6].Version)
+	ass.Equal("L", patDoc.Classifications[6].FirstLater)
+	ass.Equal("I", patDoc.Classifications[6].ClassificationValue)
+
+	ass.Equal("N", patDoc.Classifications[7].SubClass)
+	ass.Equal("9", patDoc.Classifications[7].MainGroup)
+	ass.Equal("1085", patDoc.Classifications[7].SubGroup)
+	ass.Equal("20130101", patDoc.Classifications[7].Version)
+	ass.Equal("L", patDoc.Classifications[7].FirstLater)
+	ass.Equal("I", patDoc.Classifications[7].ClassificationValue)
+
+	ass.Equal("N", patDoc.Classifications[8].SubClass)
+	ass.Equal("9", patDoc.Classifications[8].MainGroup)
+	ass.Equal("88", patDoc.Classifications[8].SubGroup)
+	ass.Equal("20130101", patDoc.Classifications[8].Version)
+	ass.Equal("L", patDoc.Classifications[8].FirstLater)
+	ass.Equal("I", patDoc.Classifications[8].ClassificationValue)
+
+	ass.Equal("N", patDoc.Classifications[9].SubClass)
+	ass.Equal("15", patDoc.Classifications[9].MainGroup)
+	ass.Equal("10", patDoc.Classifications[9].SubGroup)
+	ass.Equal("20130101", patDoc.Classifications[9].Version)
+	ass.Equal("L", patDoc.Classifications[9].FirstLater)
+	ass.Equal("I", patDoc.Classifications[9].ClassificationValue)
+
+	ass.Equal("Y", patDoc.Classifications[10].SubClass)
+	ass.Equal("205", patDoc.Classifications[10].MainGroup)
+	ass.Equal("01048", patDoc.Classifications[10].SubGroup)
+	ass.Equal("20130101", patDoc.Classifications[10].Version)
+	ass.Equal("L", patDoc.Classifications[10].FirstLater)
+	ass.Equal("A", patDoc.Classifications[10].ClassificationValue)
+
+	ass.Equal("Y", patDoc.Classifications[11].SubClass)
+	ass.Equal("403", patDoc.Classifications[11].MainGroup)
+	ass.Equal("01019", patDoc.Classifications[11].SubGroup)
+	ass.Equal("20130101", patDoc.Classifications[11].Version)
+	ass.Equal("L", patDoc.Classifications[11].FirstLater)
+	ass.Equal("A", patDoc.Classifications[11].ClassificationValue)
+
+	ass.Equal("Y", patDoc.Classifications[12].SubClass)
+	ass.Equal("404", patDoc.Classifications[12].MainGroup)
+	ass.Equal("01001", patDoc.Classifications[12].SubGroup)
+	ass.Equal("20130101", patDoc.Classifications[12].Version)
+	ass.Equal("L", patDoc.Classifications[12].FirstLater)
+	ass.Equal("A", patDoc.Classifications[12].ClassificationValue)
+
+	ass.Equal("Y", patDoc.Classifications[13].SubClass)
+	ass.Equal("404", patDoc.Classifications[13].MainGroup)
+	ass.Equal("01021", patDoc.Classifications[13].SubGroup)
+	ass.Equal("20130101", patDoc.Classifications[13].Version)
+	ass.Equal("L", patDoc.Classifications[13].FirstLater)
+	ass.Equal("A", patDoc.Classifications[13].ClassificationValue)
+}
+
 //--------------------------------------
